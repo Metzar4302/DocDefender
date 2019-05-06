ded99389291e741d03918cdcd3a47a5f356b08fd
ZakharovIA <ZakharovIA@nornik.ru>
2019-05-06 15:38:19 +0300

    Fix

--- DocDefender/Program.cs
+++ DocDefender/Program.cs
@@ -5,25 +5,27 @@ using System.Text;
 using System.Collections.Generic;
 using System.Threading;
 using Newtonsoft.Json;
+using System.Diagnostics;
 #endregion
 
 namespace DocDefender
 {
     class Program
     {
-        static Blockchain blockChain1 = new Blockchain();
+        private static Blockchain blockChain1 = new Blockchain();
         static void Main(string[] args) {
-            Document doc1 = new Document("Doc information", new List<string>(){"DinaryDocData_1", "DinaryDocData_2", "DinaryDocData_3"});
-            Document doc2 = new Document("Doc_2 information", new List<string>(){"11", "22", "33"});
-            Document doc3 = new Document("Doc_3 information", new List<string>(){"111", "222", "333"});
-            Document doc4 = new Document("Doc_4 information", new List<string>(){"1111", "2222", "3333"});
+            
+            // Document doc1 = new Document("Doc information", new List<string>(){"BinaryDocData_1", "BinaryDocData_2", "BinaryDocData_3"});
+            // Document doc2 = new Document("Doc_2 information", new List<string>(){"11", "22", "33"});
+            // Document doc3 = new Document("Doc_3 information", new List<string>(){"111", "222", "333"});
+            // Document doc4 = new Document("Doc_4 information", new List<string>(){"1111", "2222", "3333"});
+            // // Test
+            // string serialized = JsonConvert.SerializeObject(doc1);
+            // // Test error 
+            // string brokenSerialized = "{\"DocInfo\":777,\"DocsFiles\":\"Just text\"}";
 
-            string serialized = JsonConvert.SerializeObject(doc1);
-
-            string brokenSerialized = "{\"DocInfo\":777,\"DocsFiles\":\"Just text\"}";
-
-            Tools.WriteLineColorized($"{ReadData(brokenSerialized)}", ConsoleColor.Red);
-            Tools.WriteLineColorized($"{ReadData(serialized)}", ConsoleColor.Blue);
+            // Tools.WriteLineColorized($"{ReadData(brokenSerialized)}", ConsoleColor.Red);
+            // Tools.WriteLineColorized($"{ReadData(serialized)}", ConsoleColor.Blue);
             
             // Tools.ChainView(blockChain1);
         }
